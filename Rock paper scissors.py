8ef840d16a23f1c91bdcd61c139072ec02ac4cea
SATYABAN PRADHAN <satyabanpradhan159@gmail.com>
2024-12-22 18:56:14 +0530

    Update Rock paper scissors.py

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -30,27 +30,55 @@ scissors = '''
 '''
 game_images = [rock ,paper,scissors]
 
-user_choice = int(input("what do you chhose ? Type 0 for rock,1 for paper or 2 for scissors : "))
-if user_choice >= 0 and user_choice <=2:
+user_choice = int(input("what do you chhose ? Type 0 for rock,1 for paper or 2 for scissors : \n"))
+print("#Your choice:-")
+if (user_choice < 0 or  user_choice > 2 ):
+    print("You entered an invalid number, so the computer will choose randomly for you.")
+    user_choice = random.randint(0, 2)
     print(game_images[user_choice])
 
-computer_choice = random.randint(0,2)
-print("computer choose ")
-print(game_images[computer_choice])
+    print("#computer choice :- \n")
+    computer_choice = random.randint(0, 2)
+    print(game_images[computer_choice])
 
-
-if user_choice >= 3 or user_choice < 0:
-    print("you typed an invalid number . you lose!")
-elif (computer_choice == 0 and user_choice == 2):
-    print("you won")
-elif (user_choice ==0 and  computer_choice == 2):
-    print("you loose !")
-elif (computer_choice > user_choice):
-    print("you loose !")
-elif(user_choice >computer_choice):
-    print("you won!")
-elif computer_choice == user_choice:
-    ("the game is draw")
+    if user_choice == 0 and computer_choice == 2:
+        print("You Won !")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You Won !")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You Won !")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You loose !")
+    elif user_choice == 0 and computer_choice == 1:
+        print("You loose !")
+    elif user_choice == 1 and computer_choice == 2:
+        print("You loose !")
+    elif user_choice == computer_choice:
+        print("the match is draw !")
+    else:
+        print("somthing is wrong")
 
 else:
-    print("You typed an invalid number . you lose!")
+    print("\n #user choice")
+    print(game_images[user_choice])
+
+    print("#computer choice :- \n")
+    computer_choice = random.randint(0, 2)
+    print(game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You Won !")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You Won !")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You Won !")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You loose !")
+    elif user_choice == 0 and computer_choice == 1:
+        print("You loose !")
+    elif user_choice == 1 and computer_choice == 2:
+        print("You loose !")
+    elif user_choice == computer_choice:
+        print("the match is draw !")
+    else:
+        print("somthing is wrong")
